f2abd380878e6969d8149f131128d83354bb5a8e
jitingupta24 <63880919+jitingupta24@users.noreply.github.com>
2023-06-18 07:38:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region = "us-east-2"
+    
+}
+
+resource "aws_instance" "my-vm" {
+    ami = "ami-024e6efaf93d85776"
+    instance_type = "t2.micro"
+    
+
+user_data = <<-EOF
+#!/bash/bin
+apt-get update -y
+sudo apt-get install nginx -y
+sudo systemctl enable nginx
+sudo systemctl start nginx
+EOF
+
+    tags = {
+      Name = "TF_VM_Jenkins"
+    }
+  
+}
