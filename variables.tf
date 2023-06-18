2b3070eeaf327c9459c067c60d86686b4ef625f4
jitingupta24 <63880919+jitingupta24@users.noreply.github.com>
2023-06-18 08:32:19 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  description = "The type of instance to start"
+  type        = string
+  default	  = "t2.micro"
+}
+
+variable "ami" {
+  description = "ID of AMI to use for the instance"
+  type        = string
+  default	  = "ami-024e6efaf93d85776"
+}
